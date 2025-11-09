d71d71a2d560d78652d21d96a4fc8a499b987765
Yogesh Dhanapal <geoyogesh@gmail.com>
2025-11-08 23:43:29 -0500

    chore(release): bump GeoETL to 0.4.0

--- docs/geoetl-doc-site/docusaurus.config.ts
+++ docs/geoetl-doc-site/docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'GeoETL',
   tagline: 'High-Performance Geospatial Data Conversion',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/geoetl_logo2.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
